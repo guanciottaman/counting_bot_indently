532c23b3ba4f00d2be0f9c67deb02848fae538cf
guanciottaman <113635544+guanciottaman@users.noreply.github.com>
2024-03-06 21:59:06 +0100

    Added actual functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from dotenv import load_dotenv
 import discord
@@ -7,15 +8,55 @@ from discord.ext import commands
 load_dotenv('.env')
 
 TOKEN: str = os.getenv('TOKEN')
+with open('config.json', 'r') as f:
+    channel_id = json.load(f)['channel_id']
+current_count: int = 0
+current_member: discord.Member = None
 
 class Bot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix='!', intents=discord.Intents.default())
+        intents = discord.Intents.default()
+        intents.message_content = True
+        super().__init__(command_prefix='!', intents=intents)
 
     async def on_ready(self):
         print(f'Bot is ready as {self.user.name}#{self.user.discriminator}')
+    
+    async def on_message(self, message: discord.Message) -> None:
+        global current_count
+        if message.author == self.user:
+            return
+        if channel_id is None:
+            return
+        if message.channel.id != channel_id:
+            return
+        content: str = message.content.split()[0]
+        if not content.isdigit():
+            return
+        if int(content) != int(content)+1:
+            await message.channel.send(f'{message.author.mention} messed up the count! The correct number was {int(content)+1}\nRestart by 0.')
+            current_count = 0
+            return
+        if current_count and current_member == message.author:
+            await message.channel.send(f'{message.author.mention} messed up the count! You cannot count two numbers in a row!\nRestart by 0.')
+            current_count = 0
+            return
+        current_count += 1
+        await message.add_reaction(':white_check_mark:')
 
 bot = Bot()
+
+
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def set_channel(ctx: commands.Context, channel_id:int):
+    if not isinstance(channel_id, int):
+        await ctx.send('Channel ID must be an integer')
+        return
+    with open('config.json', 'w') as f:
+        json.dump({'channel_id': channel_id}, f)
+    channel: discord.TextChannel = await bot.fetch_channel(channel_id)
+    
 
 
 if __name__ == '__main__':
