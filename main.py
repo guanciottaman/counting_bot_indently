541a8cec24592cffa636b775c3cca2d11bf14852
guanciottaman <113635544+guanciottaman@users.noreply.github.com>
2024-03-06 21:33:40 +0100

    Setup

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ import discord
 from discord.ext import commands
 
 load_dotenv('.env')
+
+TOKEN: str = os.getenv('TOKEN')
+
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix='!', intents=discord.Intents.default())
+
+    async def on_ready(self):
+        print(f'Bot is ready as {self.user.name}#{self.user.discriminator}')
+
+bot = Bot()
+
+
+if __name__ == '__main__':
+    bot.run(TOKEN)
