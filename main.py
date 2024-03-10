70da6e6902bcc5da8030775f021bb8ccd57af1cf
guanciottaman <113635544+guanciottaman@users.noreply.github.com>
2024-03-10 19:09:32 +0100

    Update

--- main.py
+++ main.py
@@ -157,9 +157,12 @@ class Bot(commands.Bot):
             return
         failed_role: discord.Role = discord.utils.get(message.guild.roles, id=config.failed_role_id)
         if failed_role not in message.author.roles:
-            failed_member: discord.Member = await message.guild.fetch_member(config.current_member_id)
-            await failed_member.remove_roles(failed_role)
-            await message.author.add_roles(failed_role)
+            if config.failed_member_id is not None:
+                prev_failed_member: discord.Member = await message.guild.fetch_member(config.failed_member_id)
+                await prev_failed_member.remove_roles(failed_role) # Remove role from previous failed user
+            await message.author.add_roles(failed_role)  # Add role to current user who has failed
+            config.failed_member_id = message.author.id  # Designate current user as failed member
+            config.update()
         config.reset()
 
     async def handle_wrong_member(self, message: discord.Message) -> None:
@@ -171,9 +174,12 @@ class Bot(commands.Bot):
             return
         failed_role = discord.utils.get(message.guild.roles, id=config.failed_role_id)
         if failed_role not in message.author.roles:
-            failed_member = await message.guild.fetch_member(config.current_member_id)
-            await failed_member.remove_roles(failed_role)
-            await message.author.add_roles(failed_role)
+            if config.failed_member_id is not None:
+                prev_failed_member: discord.Member = await message.guild.fetch_member(config.failed_member_id)
+                await prev_failed_member.remove_roles(failed_role) # Remove role from previous failed user
+            await message.author.add_roles(failed_role)  # Add role to current user who has failed
+            config.failed_member_id = message.author.id  # Designate current user as failed member
+            config.update()
         config.reset()
     
         
